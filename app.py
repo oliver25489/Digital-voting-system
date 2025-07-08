c17281067ea1bee8713f54c429ebffbc8342207b
oliver25489 <oliverkosen@email.com>
2025-07-08 21:08:35 +0300

    Add initial files or description of changes

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+
+app = Flask(__name__)
+
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Oliver%40123%23@localhost/digital voting system'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+class EndUser(db.Model):
+    __tablename__ = 'end_users'
+    student_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
+    is_active = db.Column(db.Boolean, default=True)
+
+
+
+
+
+
+
+
+
+@app.route("/")
+def home():
+    return "Hello, University Voting System!"
+
+if __name__ == "__main__":
+    app.run(debug=True)
