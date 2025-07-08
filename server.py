11c3225386f89b53e09231a9a444d69a25c09089
oliver25489 <oliverkosen@email.com>
2025-07-08 19:59:30 +0300

    commit

--- server.py
+++ server.py
@@ -1,5 +1,10 @@
 from flask import Flask
 
+app = Flask(__name__)
 
-App = (__name__)
+@app.route("/")
+def home():
+    return "Hello, University Voting System!"
 
+if __name__ == "__main__":
+    app.run(debug=True)
